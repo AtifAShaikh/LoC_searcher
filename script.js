d7415311718e99158cac5a8eea169833074b347f
AtifAShaikh <atif.arifshaikh25@gmail.com>
2021-02-24 09:55:06 -0800

    switched javascript to jquery

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 var dataToDisplay;
-var subButton = document.querySelector('#sub');
-var locationInput = document.querySelector('#locationSearch');
-var queryInput = document.querySelector('#querySearch');
-var cardContainer = document.querySelector('#card_container');
-var loadingGif = document.querySelector('#loadingGif');
+var subButton = $('#sub');
+var locationInput = $('#locationSearch');
+var queryInput = $('#querySearch');
+var cardContainer = $('#card_container');
+var loadingGif = $('#loadingGif');
 
 function searchLibrary(qParam, location){
     console.log('search started');
     var urlToSearch = 'https://www.loc.gov/search/?q='+ qParam + '&fa=location:' + location +'&c=20&fo=json';
-    cardContainer.innerHTML = '';
-    loadingGif.style.display = 'block';
+    cardContainer.html('');
+    loadingGif.attr('style', 'display:block;');
     fetch(urlToSearch)
         .then(function (response){
             // console.log(JSON.parse(response));
@@ -17,7 +17,7 @@ function searchLibrary(qParam, location){
             return response.json();
         }).then(function (data){
             // console.log(data.results);
-            loadingGif.style.display = 'none';
+            loadingGif.attr('style', 'display:none;');
             dataToDisplay = data.results;
             console.log(dataToDisplay);
             displayData();
@@ -34,46 +34,50 @@ function displayData(){
         // title.textContent = element.title;
         // card.append(title);
         // $(cardContainer).append(card);
-        var card = document.createElement("div");
-        var title = document.createElement("h1");
-        var dateLocation = document.createElement("h6");
-        var description = document.createElement("p");
-        var image = document.createElement('img');
-        var linkButton = document.createElement('button');
+        var card = $('<div></div>');
+        var title = $('<h1>');
+        var dateLocation = $('<h6>');
+        var description = $('<p>');
+        var image = $('<img>');
+        var linkButton = $('<button>');
 
-        linkButton.setAttribute('type', 'button');
-        linkButton.setAttribute('class', 'btn btn-secondary col-12 col-md-2 col-lg-2');
-        linkButton.textContent = 'read more';
+        linkButton.attr('type', 'button')
+        linkButton.attr('class', 'btn btn-secondary col-12 col-md-2 col-lg-2');
+        linkButton.text('read more');
 
-        description.textContent = element.description[0];
+        description.text(element.description[0]);
 
-        dateLocation.textContent = element.date + ', ' + element.location[element.location.length-1];
+        dateLocation.text(element.date + ', ' + element.location[element.location.length-1]);
 
-        title.textContent = element.title;
+        title.text(element.title);
 
-        image.setAttribute('src', element.image_url[0])
-        image.width = '300';
+
+        image.attr('src', element.image_url[0]);
+        image.width('300');
 
         linkButton.onclick = function(){
             window.location.href = element.url;
         }
-
-        card.setAttribute('class', 'card mt-2 mb-2');
+        console.log(card);
+        card.attr('class', 'card mt-2 mb-2');
         card.append(title);
         card.append(dateLocation);
         card.append(image);
         card.append(description);
         card.append(linkButton);
         cardContainer.append(card);
+
+        
+        
     });
 }
 
 
 
-$(subButton).click(function(event){
+subButton.on('click', function(event){
     event.preventDefault();
-    var locationVal = locationInput.value;
-    var qVal = queryInput.value;
+    var locationVal = locationInput.val();
+    var qVal = queryInput.val();
     searchLibrary(qVal, locationVal);
 });
 
